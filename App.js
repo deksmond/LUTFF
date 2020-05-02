e4a12bf28475f35f9c2d957561bb4c3baf35c271
Desmond Amama <amamades@gmail.com>
2020-05-02 04:08:17 +0100

    perf: AppStack, AuthStack, createSwitchNavigator used for navigations and screen switching  feat: firebase integration for forms

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+import LoadingScreen from './screens/LoadingScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import HomeScreen from './screens/HomeScreen'
+
+import * as firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAvOyJOjoYEp_2B4Fegsc-8bJxFNqSiaA4",
+    authDomain: "lutff-e8054.firebaseapp.com",
+    databaseURL: "https://lutff-e8054.firebaseio.com",
+    projectId: "lutff-e8054",
+    storageBucket: "lutff-e8054.appspot.com",
+    messagingSenderId: "74723080054",
+    appId: "1:74723080054:web:eff03a84c7b9b576f9ac57"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const AppStack = createStackNavigator({
+      Home: HomeScreen
+  })
+
+  const AuthStack = createStackNavigator({
+      Login: LoginScreen,
+      Register: RegisterScreen
+  })
+
+  export default createAppContainer(
+      createSwitchNavigator(
+          {
+              Loading: LoadingScreen,
+              App: AppStack,
+              Auth: AuthStack
+          },
+          {
+              initialRouteName:"Loading"
+          }
+      )
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
